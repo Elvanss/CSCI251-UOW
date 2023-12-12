bee698a0fe7ad400327b3b2f14ff02a82f6b7f3e
Elvanss <91682614+Elvanss@users.noreply.github.com>
2023-12-13 09:43:16 +1100

    Delete CSCI251/Lab directory

--- CSCI251/Lab/LAB1/t1/Debug-B/mylibrary.h
+++
@@ -1,4 +0,0 @@
-void functionOne(int &one, int &two, int &three);
-int functionTwo(int one, int two, int three);
-
-
--- CSCI251/Lab/LAB1/t3/building.h
+++
@@ -1,6 +0,0 @@
-char materialAccepted();
-void materialInfo(double &width, double &height, double &depth);
-double materialCalculation(double width, double height, double depth, char material);
-void printInformation(double cost);
-
-
